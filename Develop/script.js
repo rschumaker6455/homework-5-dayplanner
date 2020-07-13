28d70b2c86a88ea5405a9b29d6eb43ba55e5189e
Ruth Schumaker <rschumaker6455@gmail.com>
2020-07-12 22:30:29 -0700

    added save to local storage feature

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,29 @@
 /* Should display date in header but isn't working*/
-var currentDay = document.getElementById('currentDay');
+var currentDay = document.getElementById('currentDay')
 
 function displayDate() {
     var now = moment().local();
     var display = now.format('MM/DD/YYYY');
     
     currentDay.textContent = display;
-
-    console.log(display) 
 };
 
 displayDate()
 
+/*Display*/
+
+var btn = document.querySelector('.btn');
+var textArea = document.querySelector('.text');
+var displayText = JSON.parse(localStorage.getItem('textArea'))
+
+function saveText() {
+    document.querySelector('.text').textContent = displayText
+}
+
+document.querySelector('.btn').addEventListener('click', function(){
+    var textArea = document.querySelector('.text').value;
+    localStorage.setItem('textArea',JSON.stringify(textArea))
+    
+})
+
+saveText()
